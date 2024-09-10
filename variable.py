be3d6a7f6c18a96ea0fd35a89d5e5a0fb4c81237
vaemchenko <v.a.emchenko@gmail.com>
2024-09-10 21:29:07 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+number_executed_task = '12'
+number_hours_spent = '1.5'
+cours_title = 'Python'
+time_per_task = (1.5 / 12)
+print('Курс:',cours_title, 'всего задач:',number_executed_task,', затрачено часов: ', number_hours_spent,' среднее время выполнения ',time_per_task)
