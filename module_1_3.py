7fe3457ea5d98484da0279fdb8dfcbee935d3e99
vaemchenko <v.a.emchenko@gmail.com>
2024-09-11 09:54:33 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = 'Viktor'
+print(name)
+age = 57
+print(age)
+new_age = (age + 1)
+print(new_age)
+is_student = 'True'
+print(is_student)
+print('Имя: ',name, 'Возраст: ',age,'Будущий возраст: ',new_age,'Студент: ',is_student)
