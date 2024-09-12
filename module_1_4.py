8df8cb821876c80dd79675dc2e966915163789b4
vaemchenko <v.a.emchenko@gmail.com>
2024-09-12 04:18:32 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input()
+last_character = my_string[-1]
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[:1])
+print(last_character)
+
+
+
+
