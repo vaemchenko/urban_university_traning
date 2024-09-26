458fde6ad7d8d99c555e0b81616a79aed5805d0e
vaemchenko <v.a.emchenko@gmail.com>
2024-09-26 12:44:58 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,57 @@
+# # Практическое задание по уроку "Функция в Python. Функция с параметром"
+# # Задача "Матрица воплоти"
+#
+# def get_matrix(n,m,value):
+#     matrix = []
+#     for i in get_matrix[[]]:
+#         if n < 1:
+#             continue
+#         n += 1
+#         if n >= 1:
+#             matrix.append
+#             break
+#         for j in matrix:
+#             if m < 1:
+#                 m+=1
+#                 continue
+#             if m >= 1:
+#                 matrix.append([m,value])
+#                 break
+#         print(matrix)
+# # result1 = get_matrix(2,2,10)
+# # result2 = get_matrix(3,5,42)
+# # result3 = get_matrix(4,2,13)
+# # print(result1)
+# # print(result2)
+# # print(result3)
+# from module_2_3 import my_list
+
+
+# def get_matrix(n,m,value):
+#     matrix = []
+#     for i in matrix[0]:
+#         if n <= 0:
+#             continue
+#         if n > 0:
+#             matrix[[]] * n
+#             f = True
+#             for j in i:
+#                 if m <= 0:
+#                     continue
+#                 if m > 0:
+#                     p = [] = [value] * m
+#                     break
+#     print(matrix[[p]*n])
+# result1 = get_matrix(4,2,13)
+# print(result1)
+
+def get_matr():
+    for i in matr:
+        print(i)
+result1 = matr = [[[10] * 2] * 2]
+result2 = matr = [[[42] * 5] * 3]
+result3 = matr = [[[13] * 2] * 4]
+print(result1)
+print(result2)
+print(result3)
+get_matr()
