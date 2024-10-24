411de2a9cca3bb88598f7fead0b508a3f39bdd79
vaemchenko <v.a.emchenko@gmail.com>
2024-10-24 09:56:33 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+def divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    elif first / second:
+       return (first / second)
+
+
+
+
+
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+     return inf
+    elif first / second:
+       return (first / second)
+
